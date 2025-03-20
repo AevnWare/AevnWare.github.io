9ba9ba79bbf28f08125a3bc354b50a440c53d8df
Sebastian Solace <141687545+SebaztianSolace@users.noreply.github.com>
2025-03-19 19:14:55 -0700

    Update Loader.lua

--- Scripts/Loader.lua
+++ Scripts/Loader.lua
@@ -1,2 +1,14 @@
-warn("haiii")
-warn(" :3 ")
+--// Loader for Eluvium
+
+
+
+local GameId = game.GameId
+
+
+if GameId == 3345149486 then
+	-- PVP Sword Fighting
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Eluvium-gg/Eluvium-gg.github.io/refs/heads/main/Scripts/Games/PVP-Sword-Fighting.lua"))()
+elseif GameId == 0 then
+	-- hi
+	print("a")
+end
