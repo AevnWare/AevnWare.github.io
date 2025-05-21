e7800aaed875f9dcb7d40a2d43cbd3e8515734fe
Sebastian Solace <141687545+SebaztianSolace@users.noreply.github.com>
2025-05-21 12:12:52 -0700

    Update Loader.lua

--- Scripts/Loader.lua
+++ Scripts/Loader.lua
@@ -1,19 +1,25 @@
---// Eluvium Loader.
---// Get better at games, I would say -Echelon
+--// AevnWare Loader.
+--// Dog little kids on a lego game -Elliot
 --// Join us: discord.gg/umPbSwgRM5
+
 
 --// game list, with place id's so we can find stuff. I remade this loader so MANY times.
 local gameList = {
-	[8832438757] = "https://Eluvium-gg.github.io/Scripts/Games/PVP-Sword-Fighting.lua", -- PVP Sword Fighting
-	[9377953133] = "https://Eluvium-gg.github.io/Scripts/Games/PVP-Sword-Fighting.lua", -- Mobile Sword Fight
-	[189707] = "https://Eluvium-gg.github.io/Scripts/Games/Natural-Disaster-Survival.lua", -- Natural Disaster Survival
+	[8832438757] = "https://AevnWare.github.io/Scripts/Games/PVP-Sword-Fighting.lua", -- PVP Sword Fighting
+	[9377953133] = "https://AevnWare.github.io/Scripts/Games/PVP-Sword-Fighting.lua", -- Mobile Sword Fight (A different script will be made for this game, so You can do more.)
+	[189707] = "https://AevnWare.github.io/Scripts/Games/Natural-Disaster-Survival.lua", -- Natural Disaster Survival
+}
+
+local BetaList = {
+	["Universal"] = "https://AevnWare.github.io/Scripts/Universal.lua", -- This is where like, all the testing features go, This requires manual loading.
+	[0] = "https://AevnWare.github.io/Scripts/Games/Mobile-Sword-Fight.lua", -- Im currently working on this, an update will be pushed for this.
 }
 
 --// its in the name dumbfo
 local gameFound = false
 
 --// Just loops, no need to explain honestly
-for placeId, scriptUrl in pairs(gameList) do
+for placeId, scriptUrl in pairs(gameList and _G.UseExper ~= true or BetaList) do
 	if game.PlaceId == placeId then
 		local yippee, bruh = pcall(function()
 			loadstring(game:HttpGet(scriptUrl))()
@@ -24,13 +30,13 @@ for placeId, scriptUrl in pairs(gameList) do
 			break
 		else
 			game:GetService("StarterGui"):SetCore("SendNotification", {
-				Title = "Eluvium",
-				Text = "report this to a Dev or Echelon",
+				Title = "AevnWare",
+				Text = "report this to a Dev or Elliot",
 				Duration = 5,
 				Icon = "http://www.roblox.com/asset/?id=13321880274",
 			})
 			game:GetService("StarterGui"):SetCore("SendNotification", {
-				Title = "Eluvium",
+				Title = "AevnWare",
 				Text = "I have fallen and cant get back up, Error: " .. bruh,
 				Duration = 15,
 				Icon = "http://www.roblox.com/asset/?id=13321880274",
@@ -43,10 +49,11 @@ end
 
 --// Notify if no game was found.
 if not gameFound then
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = "Eluvium",
-		Text = "No supported game found! DM a dev or Echelon if this is a mistake.",
+	--[[game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = "AevnWare",
+		Text = "No supported game found! DM a dev or Elliot if this is a mistake.",
 		Duration = 10,
 		Icon = "http://www.roblox.com/asset/?id=13321880274",
-	})
+	})]]
+	loadstring(game:HttpGet(BetaList["Universal"]))()
 end
